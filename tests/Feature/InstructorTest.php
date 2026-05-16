dcda4b208fdb504558c2f4912e03f74b0ce3e6fc
ekS12 <nignakov1@mail.ru>
2026-05-16 19:22:53 +0500

    tets1

--- tests/Feature/InstructorTest.php
+++ tests/Feature/InstructorTest.php
@@ -117,7 +117,7 @@ class InstructorTest extends TestCase
             'title' => 'Чужой МК',
             'description' => 'Описание чужого МК длиннее 10 символов',
             'date' => Carbon::now()->addDay()->format('Y-m-d'),
-            'start_time' => '13:00',
+            'start_time' => '13:00:00',
             'max_participants' => 5,
             'price' => 1000,
         ]);
